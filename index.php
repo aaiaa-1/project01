fd3c167c527f0694baffa3fc328e3a0cbff0e814
aaiaa-1 <117121490+aaiaa-1@users.noreply.github.com>
2023-03-01 14:42:19 +0100

    welcome message

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
     include_once 'header.php';
 ?>
     <div class="wrapper">
+         <?php
+            if (isset($_SESSION["userid"])){
+                echo '<p>Welcome back '.$_SESSION["useruid"].' </p>';
+            }
+        ?>
         <h1>This is nothing</h1>
     </div>
 <?php
